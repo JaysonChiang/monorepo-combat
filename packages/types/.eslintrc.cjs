ee5a1922960397535c044949e5b28bd6f7df1198
Jayson Chiang <jayson.chiang@ibm.com>
2024-12-08 14:56:55 +0800

    2.5 install prettier and global items

---
+++ packages/types/.eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: "@skimhugo/eslint-config-ts",
+  globals: {
+    // myGlobal: false,
+  },
+  rules: {
+    // common
+    // import
+    "import/first": "off",
+    // ts
+  },
+};
